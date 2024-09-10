f5cddc54aadf2346c5c980924b9b9818dd75ac90
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-09-10 19:02:05 +0100

    Added code in ContentView to use the NetworkManager and set View according to connection status.

--- NetworkManagerSwiftUI/ContentView.swift
+++ NetworkManagerSwiftUI/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemBlue).ignoresSafeArea()
+            
+            if !networkManager.isConnected {
+                Color(.systemRed).ignoresSafeArea()
+            }
+           
+            
+            VStack {
+                Image(systemName:networkManager.connectionImageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    //.foregroundColor(.white)
+                
+                Text(networkManager.connectionDescription)
+                    .font(.system(size: 24))
+                    .padding()
+                    //.foregroundColor(.white)
+                
+                if !networkManager.isConnected {
+                    Button {
+                        // Action not really needed here as NetworkManager monitor will constantly be polling.  But could some specific login here
+                        print("Handle action")
+                    } label: {
+                        Text("Retry")
+                            .padding()
+                            .font(.headline)
+                            .foregroundColor(Color(.systemBlue))
+                    }
+                    .frame(width: 140)
+                    .background(.white)
+                    .clipShape(.capsule)
+                    .padding()
+                }
+                
+            }
+            .foregroundColor(.white)
         }
-        .padding()
+       
     }
 }
 

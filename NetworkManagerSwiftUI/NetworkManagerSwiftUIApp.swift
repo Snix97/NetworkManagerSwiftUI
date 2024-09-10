3b523d60aadb0c00371e0a8c8f9395d0e3ed2eab
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2024-09-10 17:31:02 +0100

    Project setup

---
+++ NetworkManagerSwiftUI/NetworkManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerSwiftUIApp.swift
+//  NetworkManagerSwiftUI
+//
+//  Created by Claire Roughan on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
